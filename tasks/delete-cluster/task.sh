bcc154c42f3b78aef4f228ae38eae123dfa9f130
Rahul Jain <rahulkj@gmail.com>
2020-09-12 15:52:10 -0500

    Adding delete cluster flow

---
+++ tasks/delete-cluster/task.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+
+login_tkgi() {
+  pks_password=$(om -t "${OM_TARGET}" credentials -p pivotal-container-service -c ".properties.uaa_admin_password" -t json | jq -r '.secret')
+
+  tkgi login -a ${PKS_API_ENDPOINT} -u admin -p ${pks_password} -k
+}
+
+login_tkgi
+
+existing_clusters=$(tkgi clusters --json | jq -r '.[].name')
+
+defined_clusters=$(yq r repository/${ENV}/clusters/clusters.yaml -j | jq -r '.clusters[]')
+protected_clusters=$(yq r repository/${ENV}/clusters/clusters.yaml -j | jq -r '.protected_clusters[]')
+
+is_protected_cluster() {
+  is_protected=false
+  for protected_cluster in ${protected_clusters}; do
+    if [[ "${protected_cluster}" == "${1}" ]]; then
+      is_protected=true
+      break
+    fi
+  done
+  echo "${is_protected}"
+}
+
+is_removed_cluster() {
+  is_removed=true
+  for defined_cluster in ${defined_clusters}; do
+    if [[ "${defined_cluster}" == "${1}" ]]; then
+      is_removed=false
+    fi
+  done
+  echo "${is_removed}"
+}
+
+for cluster in ${existing_clusters}; do
+  is_protected=$(is_protected_cluster ${cluster})
+  is_removed=$(is_removed_cluster ${cluster})
+
+  if [ "${is_protected}" = false && "${is_removed}" = true ]; then
+    echo "tkgi delete-cluster ${cluster} --non-interactive"
+  fi
+done
